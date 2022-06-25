5ee51d7b165dabaa736df4957180d26739eacee3
Eric-Josh <techjosmar@gmail.com>
2022-06-25 08:42:29 +0100

    livestream edit

--- vue/src/views/livestream/edit.vue
+++ vue/src/views/livestream/edit.vue
@@ -23,14 +23,25 @@
             <!-- main section -->
             <div class="grid xl:grid-cols-12 xl:gap-6">
                 <div class="col-span-6">
-                    <div v-if="model.streamKey != ''">
+                    <div v-if="streamStatus == 1" class="flex justify-center py-20">
+                        <svg role="status" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" 
+                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                    </div>
+                    <div v-if="streamStatus == 2">
                         <video-player 
                             :options="videoOptions"
                             :shareOptions="share"
                             :showShare="videoOptionsCustom.share"
                             :showTitle="videoOptionsCustom.title"
-                            
                         />
+                    </div>
+                    <div v-if="streamStatus == 3">
+                        <div class="p-20 w-full bg-black border shadow-md sm:p-36 dark:bg-gray-800 dark:border-gray-700 text-center">
+                            <span class="text-red-700 font-medium font-bolder text-4xl">OFFLINE</span>
+                        </div>
                     </div>
                     <div class="p-4 w-full mt-8 bg-white border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                         <h5 class="mb-2 text-xl font-semibold text-gray-900 dark:text-white">Streaming Settings</h5>
@@ -537,7 +548,7 @@ let model = ref({
     bsignal: false,
     record_stream: null,
     streamKey: '',
-    rtmpUrl: 'rtmp://live.viloud.tv:5222/app',
+    rtmpUrl: 'rtmp://192.168.43.142:2446/app',
     liveStatus: '',
 });
 let checkboxAllChannel = ref(false);
@@ -625,8 +636,10 @@ const shareUrl = router.resolve({
     name: 'LiveShare',
     params: { lhash: route.params.hash}
 });
+const streamStatus = ref(0)
 
 const getLiveStreamContent = () => {
+    streamStatus.value = 1;
     store
         .dispatch('liveStreamEdit', route.params.hash)
         .then((res) => {
@@ -645,6 +658,7 @@ const getLiveStreamContent = () => {
                 share.value.title = `Watch "${model.value.title}" Live on `;
 
                 videoOptions.value.sources[0].src = `http://192.168.43.142:3070/hls/${data.stream_key}.m3u8`;
+                checkStreamUri();
             }
         })
         .catch((err) => {
@@ -691,16 +705,37 @@ const updateStream = async () => {
     })
 }
 
-const checkStreamUri = async () => {
-    setTimeout(() => {
-        fetch(`http://192.168.43.142:3070/hls/${model['_rawValue'].streamKey}.m3u8`)
-        .then((res) => {
-            console.log(res)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }, 2000)
+
+const checkStreamUri = () => {
+    let lopper = () => {
+        setTimeout( async () => {
+            await fetch(`http://192.168.43.142:3070/hls/${model['_rawValue'].streamKey}.m3u8`)
+            .then(res => res)
+            .then(data => {
+                if(data.status) {
+                    if(data.status == 404) {
+                        streamStatus.value = 3;
+                        lopper();
+                    } else if(data.status == 200) {
+                        streamStatus.value = 2;
+                        // check live status if is not there then download video
+                        lopper();
+                    } else if(data.status == 500) {
+                        streamStatus.value = 3;
+                        lopper();
+                    }
+                }
+                // console.log(`from fetch ${data.status}`)
+            })
+            .catch(err => {
+                streamStatus.value = 3;
+                lopper();
+                console.log(`error ${err}`)
+            })
+
+        }, 10000)
+    }
+    lopper();
 }
 
 const broadcastSignal = () => {
